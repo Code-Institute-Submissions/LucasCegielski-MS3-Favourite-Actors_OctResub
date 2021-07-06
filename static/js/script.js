85d435b79ceeb43d7c4e39cd0ccbb6be2ff59225
LucasCegielski <lucas.cegielski@yahoo.ie>
2021-07-06 09:47:28 +0000

    add the add actors template with Java functions

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
+    $('select').formSelect();
+    $('.datepicker').datepicker({
+      format: 'dd mmmm, yyyy',
+      yearRange: [1300, 2000],
+      showClearBtn: true,
+      i18n: {
+        done: 'Select'
+      }
+    });
   });
